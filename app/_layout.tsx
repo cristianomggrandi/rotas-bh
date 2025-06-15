7689b24f847e9abf5bc50fab5fd625cb050d7a4b
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2025-06-15 12:01:26 -0300

    Criar switches de prioridades

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router"
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: "Rota" }} />
+            <Stack.Screen name="index" options={{ title: "Rotas BH" }} />
             <Stack.Screen name="directions" options={{ title: "Direções" }} />
         </Stack>
     )
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "expo-router"
-import Openrouteservice from "openrouteservice-js"
 import { useState } from "react"
-import { Button, FlatList, Pressable, StyleSheet, Text, TextInput, View } from "react-native"
+import { Button, FlatList, Pressable, StyleSheet, Switch, Text, TextInput, View } from "react-native"
 import { searchWithText } from "../util/services"
 
 export default function Index() {
@@ -14,6 +13,10 @@ export default function Index() {
     const [fim, setFim] = useState("")
     const [fimCoordinates, setFimCoordinates] = useState<number[]>()
     const [searchOptions, setSearchOptions] = useState<any[]>([])
+
+    const [priorizarElevacao, setPriorizarElevacao] = useState(false)
+    const [priorizarIluminacao, setPriorizarIluminacao] = useState(false)
+    const [priorizarPavimentacao, setPriorizarPavimentacao] = useState(false)
 
     return (
         <View style={styles.page}>
@@ -60,38 +63,55 @@ export default function Index() {
             />
 
             <View style={styles.list_container}>
-                <FlatList
-                    data={searchOptions}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => (
-                        <Pressable
-                            style={styles.item}
-                            onPress={() => {
-                                switch (focusedInput) {
-                                    case "inicio":
-                                        setInicio(item.properties.label)
-                                        setInicioCoordinates(item.geometry.coordinates)
+                {searchOptions.length ? (
+                    <FlatList
+                        data={searchOptions}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => (
+                            <Pressable
+                                style={styles.item}
+                                onPress={() => {
+                                    switch (focusedInput) {
+                                        case "inicio":
+                                            setInicio(item.properties.label)
+                                            setInicioCoordinates(item.geometry.coordinates)
 
-                                        setFocusedInput("fim")
-                                        break
+                                            setFocusedInput("fim")
+                                            break
 
-                                    case "fim":
-                                        setFim(item.properties.label)
-                                        setFimCoordinates(item.geometry.coordinates)
-                                        break
+                                        case "fim":
+                                            setFim(item.properties.label)
+                                            setFimCoordinates(item.geometry.coordinates)
+                                            break
 
-                                    default:
-                                        throw new Error("Erro: focusedInput inválido")
-                                }
+                                        default:
+                                            throw new Error("Erro: focusedInput inválido")
+                                    }
 
-                                setSearch("")
-                                setSearchOptions([])
-                            }}
-                        >
-                            <Text style={styles.text}>{item.properties.label}</Text>
-                        </Pressable>
-                    )}
-                />
+                                    setSearch("")
+                                    setSearchOptions([])
+                                }}
+                            >
+                                <Text style={styles.text}>{item.properties.label}</Text>
+                            </Pressable>
+                        )}
+                    />
+                ) : (
+                    <View>
+                        <View style={styles.switch_container}>
+                            <Text style={styles.switch_label}>Priorizar menor elevação</Text>
+                            <Switch value={priorizarElevacao} onValueChange={setPriorizarElevacao} />
+                        </View>
+                        <View style={styles.switch_container}>
+                            <Text style={styles.switch_label}>Priorizar iluminação</Text>
+                            <Switch value={priorizarIluminacao} onValueChange={setPriorizarIluminacao} />
+                        </View>
+                        <View style={styles.switch_container}>
+                            <Text style={styles.switch_label}>Priorizar pavimentação</Text>
+                            <Switch value={priorizarPavimentacao} onValueChange={setPriorizarPavimentacao} />
+                        </View>
+                    </View>
+                )}
             </View>
         </View>
     )
@@ -136,5 +156,16 @@ const styles = StyleSheet.create({
         padding: 16,
         borderBottomColor: "#ccc",
         borderBottomWidth: 1,
+    },
+    switch_container: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    switch_label: {
+        fontSize: 16,
+        color: "#333",
     },
 })
